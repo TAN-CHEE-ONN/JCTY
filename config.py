8823a667882af65f0cac35433d5bf06087062a76
TAN-CHEE-ONN <77651630+TAN-CHEE-ONN@users.noreply.github.com>
2022-04-06 17:06:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+customhost = "employee.cenumlmfsqnw.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tancheeonn-bucket"
+customregion = "us-east-1"
+cloudfront = "dax16l2qkp7po.cloudfront.net"
+
+
